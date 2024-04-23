ccdda55fa2c4e003529eaa82df9fd9c7c8d7b402
backspacev9 <backspacev9@gmail.com>
2024-04-24 01:26:08 +0300

    feat: make some design of main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
+      <div className="bg-container">
+        <img src="./images/bg-forest.jpg" className="bg-main" alt="" />
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="search-box">
+          <img src="./icons/search.svg" alt="search" />
+          <input type="text" placeholder="Find your City" />
+        </div>
       </header>
+
+      <main>
+        <div className="weather-container">
+          <div className="current-weather-block">
+            <div className="block">
+              <span className="current-temperature">11°C</span>
+              <span className="location">Vitebsk, Belarus</span>
+              <span className="date">Tuesday - 24.04.2024</span>
+            </div>
+            <div className="block">
+              <img src="./icons/clouds.svg" alt="cloud" />
+              <span className="time">9:00</span>
+            </div>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
